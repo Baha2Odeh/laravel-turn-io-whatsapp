f89e46ef10c96f3c59f5c7c0560f0cd2caef5890
Bahaa Odeh <bw4@hotmail.it>
2025-02-02 12:29:48 +0300

    Update MessageRequestException.php

--- src/Exceptions/MessageRequestException.php
+++ src/Exceptions/MessageRequestException.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Client\Response;
 
 class MessageRequestException extends \Exception
 {
-    public array $error;
+    public array $errors;
 
     public function __construct(Response $response)
     {
-        $message = $response->json('error.message', match ($response->status()) {
+        $message = $response->json('errors.0.error_data.details', $response->json('errors.0.message',match ($response->status()) {
             404 => 'Resource not found',
             429 => 'Too many requests',
             default => 'Error',
-        });
+        }));
 
-        parent::__construct($message, $response->json('error.code'));
-        $this->error = $response->json('error', []);
+        parent::__construct($message, $response->json('errors.0.code'));
+        $this->errors = $response->json('errors', []);
     }
 }
